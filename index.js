3f48e44cd54c94c1364b50093bdb8e046e3c76e5
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2019-06-06 19:04:03 +0900

    add focus,blor event check

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ window.addEventListener('load', function () {
   consoleNode.innerHTML = consoleStack.join('</br>');
 })
 
+window.addEventListener('focus', function () {
+  const message = "browser-event-checker: focus!";
+  console.log(message);
+  consoleStack.push(message)
+  consoleNode.innerHTML = consoleStack.join('</br>');
+})
+
 window.addEventListener('beforeunload', function (event) {
   event.preventDefault();
   const message = "browser-event-checker: beforeunload!";
@@ -23,6 +30,16 @@ window.addEventListener('beforeunload', function (event) {
 window.addEventListener('pagehide', function (event) {
   event.preventDefault();
   const message = "browser-event-checker: pagehide!";
+  console.log(message);
+  consoleStack.push(message)
+  consoleNode.innerHTML = consoleStack.join('</br>');
+  window.localStorage.setItem('consoleStack', consoleStack)
+  event.returnValue = '';
+})
+
+window.addEventListener('blur', function (event) {
+  event.preventDefault();
+  const message = "browser-event-checker: blur!";
   console.log(message);
   consoleStack.push(message)
   consoleNode.innerHTML = consoleStack.join('</br>');
