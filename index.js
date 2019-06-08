c251c664aaa634a4345a22215ced17bc18e4a9cb
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2019-06-08 16:06:57 +0900

    fix message

--- index.js
+++ index.js
@@ -23,53 +23,53 @@ function setConsoleStackToNode() {
 
 window.addEventListener('pageshow', function(event) {
   if (event.persisted) {
-    const message = "browser-event-checker: pageshow! source from cache";
+    const message = "browser-event-checker: window pageshow! source from cache";
     console.log(message);
     pushLocalStrage(message).then(setConsoleStackToNode);
   } else {
-    const message = "browser-event-checker: pageshow! source from server";
+    const message = "browser-event-checker: window pageshow! source from server";
     console.log(message);
     pushLocalStrage(message).then(setConsoleStackToNode);
   }
 })
 
 window.addEventListener('load', function () {
-  const message = "browser-event-checker: load!";
+  const message = "browser-event-checker: window load!";
   console.log(message);
   pushLocalStrage(message).then(setConsoleStackToNode);
 })
 
 window.addEventListener('focus', function () {
-  const message = "browser-event-checker: focus!";
+  const message = "browser-event-checker: window focus!";
   console.log(message);
   pushLocalStrage(message).then(setConsoleStackToNode);
 })
 
 window.addEventListener('beforeunload', function () {
-  const message = "browser-event-checker: beforeunload!";
+  const message = "browser-event-checker: window beforeunload!";
   console.log(message);
   pushLocalStrage(message).then(setConsoleStackToNode);
 })
 
 window.addEventListener('pagehide', function () {
-  const message = "browser-event-checker: pagehide!";
+  const message = "browser-event-checker: window pagehide!";
   console.log(message);
   pushLocalStrage(message).then(setConsoleStackToNode);
 })
 
 window.addEventListener('blur', function () {
-  const message = "browser-event-checker: blur!";
+  const message = "browser-event-checker: window blur!";
   console.log(message);
   pushLocalStrage(message).then(setConsoleStackToNode);
 })
 
 document.addEventListener("visibilitychange", function() {
   if (document.hidden) {
-    const message = "browser-event-checker: document hidden!";
+    const message = "browser-event-checker: document visibilitychange(hidden!)";
     console.log(message);
     pushLocalStrage(message).then(setConsoleStackToNode);
   } else {
-    const message = "browser-event-checker: document visible!";
+    const message = "browser-event-checker: document visibilitychange(visible!)";
     console.log(message);
     pushLocalStrage(message).then(setConsoleStackToNode);
   }
